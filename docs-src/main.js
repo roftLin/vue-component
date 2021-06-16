1e29ca6fa3cecfc3a78ea36fd776b55c4fcf15c8
linyi <linyi039@haluobike.com>
2021-06-16 20:00:45 +0800

    auto commit

--- docs-src/main.js
+++ docs-src/main.js
@@ -3,11 +3,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import {install} from '../src'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import locale from 'element-plus/lib/locale/lang/zh-cn'
+import 'dayjs/locale/zh-cn'
+
 import('../package.json').then(config => import(`../dist/${config.name}.css`))
 
 const app = createApp(App)
 app.use(install)
 app.use(router)
+app.use(ElementPlus, { locale, size: 'mini' });
 
 app.mount('#app')
 
--- docs-src/views/Home.vue
+++ docs-src/views/Home.vue
@@ -2,7 +2,25 @@
   <div class="page-home page">
     <section class="demo">
       <div class="section-content">
-        <v-component msg="vue component" />
+        <!-- <v-component msg="vue component" /> -->
+        <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
       </div>
     </section>
 
@@ -43,7 +61,24 @@ export default {
   data() {
     return {
       mainSnippet,
-      componentSnippet
+      componentSnippet,
+      tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }]
     }
   }
 }
