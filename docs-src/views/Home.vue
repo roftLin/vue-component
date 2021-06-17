2f1b9d24dd7e7e379de2426842ccc7cfe8fa0b07
linyi <linyi039@haluobike.com>
2021-06-17 17:30:39 +0800

    auto commit

--- docs-src/views/Home.vue
+++ docs-src/views/Home.vue
@@ -2,8 +2,8 @@
   <div class="page-home page">
     <section class="demo">
       <div class="section-content">
-        <!-- <v-component msg="vue component" /> -->
-        <el-table
+        <EditTable :config="config" @submit="submit" style="margin:20px;" />
+        <!-- <el-table
       :data="tableData"
       style="width: 100%">
       <el-table-column
@@ -20,7 +20,7 @@
         prop="address"
         label="地址">
       </el-table-column>
-    </el-table>
+    </el-table> -->
       </div>
     </section>
 
@@ -51,6 +51,23 @@ data () {
 const componentSnippet = `
 <v-component :msg="msg" />
 `
+
+const repayTypeList = {
+   averageCapital: '等额本金',
+   averageInterest: '等额本息'
+},
+columns = [
+  { prop: 'repaymentMethod', label: '还款方式', attr: {width: '180'}, format: ({ repaymentMethod }) => repayTypeList[repaymentMethod]},
+  { prop: 'productPer', label: '期数', attr: {width: '180'}, format: ({ productPer }) => `${+ productPer + 1}期(${productPer}个月)` },
+  { prop: 'costRate', label: '成本利率', attr: {minWidth: '110'}, edit: true, type: 'select', options: [{label: '5%', value: '5'}, {label: '10%', value: '10'}] },
+  { prop: 'price', label: '单价', attr: {minWidth: '110'}, edit: true },
+  { prop: 'company', label: '所属公司', attr: {minWidth: '110'}, edit: true },
+  { prop: 'product', label: '产品', attr: {minWidth: '110'}, edit: true, type: 'checkbox', options: [{label: '橘子', value: 'orange'}, {label: '苹果', value: 'apple'}] },
+  { prop: 'date', label: '日期', attr: {minWidth: '110'}, edit: true, type: 'date', required: false },
+  { prop: 'lock', label: '锁定', attr: {minWidth: '110'}, edit: true, type: 'switch' },
+  { prop: 'opt', label: '操作', attr: {minWidth: '110'}, edit: true },
+]
+
 export default {
   name: 'Home',
   components: {
@@ -78,7 +95,24 @@ export default {
             date: '2016-05-03',
             name: '王小虎',
             address: '上海市普陀区金沙江路 1516 弄'
-          }]
+          }],
+          config: {
+        columns,
+        data: [],
+      },
+
+    }
+  },
+  created(){
+    this.config.data = [
+      {repaymentMethod: 'averageCapital', productPer: '1', price: '5', company: '谷歌上海', date: '2021-01-03', opt: false},
+      {repaymentMethod: 'averageInterest', productPer: '3', costRate: '10', price: '', company: '雅虎北京', opt: true},
+      {repaymentMethod: 'averageInterest', productPer: '5', costRate: '5', price: '100', company: '上海你真美', opt: false},
+    ]
+  },
+  methods: {
+    submit(res){
+      console.log(res)
     }
   }
 }
--- src/lib/component.vue
+++ src/lib/component.vue
@@ -1,23 +1,278 @@
 <template>
-  <div class="component">
-    <div class="component-content">{{ msg }}</div>
-  </div>
+  <el-form :model="form"
+           ref="form"
+           size="small"
+           :rules="rules">
+    <el-form-item v-if="!isDetail">
+      <el-button type="primary"
+                 @click="add">新增</el-button>
+    </el-form-item>
+    <el-table :data="form.list"
+              border>
+      <el-table-column v-for="item in columns"
+                       :key="item.prop"
+                       :label="item.label"
+                       :prop="item.prop"
+                       v-bind="item.attr">
+        <template #default="{row, $index}">
+          <t-text v-if="!item.edit"
+                  :row="{item, row}" />
+          <template v-else>
+            <t-text v-if="isDetail"
+                    :row="{item, row}" />
+            <template v-else>
+              <template v-if="item.prop=='opt'">
+                <el-link type="primary"
+                         :underline="false"
+                         @click="save(row, $index)">保存</el-link>
+                <el-link type="primary"
+                         :underline="false"
+                         v-if="row.isAdd"
+                         @click="del($index)">删除</el-link>
+                <el-link type="primary"
+                         :underline="false"
+                         @click="resetField($index)">重置</el-link>
+              </template>
+              <t-input v-else
+                       v-model="row[`${item.prop}`]"
+                       v-bind="componentAttrs(item, $index)"
+                       class="width100" />
+            </template>
+          </template>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-form-item v-if="!isDetail">
+      <template v-if="isSubmit">
+        <el-button type="primary"
+                   @click="submit">提交</el-button>
+        <el-button @click="reset">重置</el-button>
+      </template>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
+import { h } from 'vue'
+
 export default {
-  name: 'VComponent',
-  props: {
-    msg: { type: String, default: '' },
+  name: 'EditTable',
+  props: { config: Object },
+  components: {
+    TInput: {
+      // functional: true,
+      props: ['prop', 'rules', 'type', 'options'],
+      render: ({
+        prop,
+        rules,
+        type = 'default',
+        options = [],
+        attrs,
+        emit: { input = () => {} },
+      }) => {
+        debugger
+        const children = {
+          checkbox: () =>
+            h(
+              'el-checkbox-group',
+              {
+                ...attrs,
+                on: {
+                  input(v) {
+                    input(v)
+                  },
+                },
+              },
+              options.map((o) =>
+                h('el-checkbox', { ...o, label: o.value, key: o.value }, [
+                  o.label,
+                ])
+              )
+            ),
+          select: () =>
+            h(
+              'el-select',
+              {
+                class: 'width100',
+                ...attrs,
+                on: {
+                  change(v) {
+                    input(v)
+                  },
+                },
+              },
+              options.map((o) => h('el-option', { ...o, key: o.value }))
+            ),
+          date: () =>
+            h('el-date-picker', {
+              type: 'date',
+              valueFormat: 'yyyy-MM-dd',
+              ...attrs,
+            }),
+          switch: () => h('el-switch', { activeColor: '#13ce66', ...attrs }),
+          opt: () => '-',
+          default: () => h('el-input', attrs),
+        }
+
+        return h('el-form-item', { prop, rules }, [children[type](h)])
+      },
+    },
+    // TText: {
+    //   functional: true,
+    //   props: ['row'],
+    //   render: (
+    //     {
+    //       props: {
+    //         row: { item, row },
+    //       },
+    //     }
+    //   ) => {
+    //     if (!row[`${item.prop}`]) return h('span', '-')
+    //     else if (item.format && typeof item.format == 'function')
+    //       return h('span', item.format(row))
+    //     else return h('span', row[`${item.prop}`])
+    //   },
+    // },
+  },
+  data() {
+    const { columns = [], data = [], isDetail = false, isSubmit = false } =
+      this.config || {}
+
+    return {
+      form: {
+        list:
+          data && data.length > 0
+            ? data.map((n) =>
+                columns.reduce(
+                  (r, c) => ({
+                    ...r,
+                    [c.prop]:
+                      n[c.prop] == false
+                        ? n[c.prop]
+                        : n[c.prop] || (c.type == 'checkbox' ? [] : ''),
+                  }),
+                  {}
+                )
+              )
+            : [
+                columns.reduce(
+                  (r, c) => ({
+                    ...r,
+                    [c.prop]:
+                      c.type == 'checkbox'
+                        ? []
+                        : c.type == 'switch'
+                        ? false
+                        : '',
+                  }),
+                  { isAdd: true }
+                ),
+              ],
+      },
+      columns,
+      rules: columns.reduce(
+        (r, c) => ({
+          ...r,
+          [c.prop]: {
+            required: c.required == false ? false : true,
+            message: c.label + '必填',
+            trigger: 'blur',
+          },
+        }),
+        {}
+      ),
+      isDetail,
+      isSubmit,
+    }
+  },
+  methods: {
+    componentAttrs(item, idx) {
+      const { type, label } = item,
+        attrs = Object.fromEntries(
+          Object.entries(item).filter(
+            (n) => !/^(prop|edit|label|attr|format)/.test(n[0])
+          )
+        ),
+        placeholder =
+          (/^(select|el-date-picker)/.test(type) ? '请选择' : '请输入') + label
+      Object.assign(attrs, {
+        prop: `list.${idx}.${item.prop}`,
+        rules: this.rules[item.prop],
+      })
+      return { ...attrs, placeholder }
+    },
+    add() {
+      const { columns = [] } = this.config || {},
+        obj = columns.reduce(
+          (r, c) => ({
+            ...r,
+            [c.prop]:
+              c.type == 'checkbox' ? [] : c.type == 'switch' ? false : '',
+          }),
+          { isAdd: true }
+        )
+      this.form.list.push(obj)
+    },
+    save(row, idx) {
+      let ret = Object.keys(row)
+          .map((r) => `list.${idx}.${r}`)
+          .filter((r) => !/isAdd|opt/g.test(r)),
+        {
+          $refs: { form },
+        } = this,
+        num = 0
+
+      form.validateField(ret, (valid) => {
+        if (valid) {
+          num++
+        }
+      })
+
+      if (num == 0)
+        this.$emit(
+          'submit',
+          Object.fromEntries(
+            Object.entries(row).filter((n) => !/^(isAdd|opt)/.test(n[0]))
+          )
+        )
+    },
+    del(idx) {
+      this.form.list.splice(idx, 1)
+      this.$refs.form.fields.forEach((n) => {
+        if (n.prop.split('.')[1] == idx) {
+          n.clearValidate()
+        }
+      })
+    },
+    submit() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          this.$emit(
+            'submit',
+            this.form.list.map((m) =>
+              Object.fromEntries(
+                Object.entries(m).filter((n) => !/^(isAdd|opt)/.test(n[0]))
+              )
+            )
+          )
+        }
+      })
+    },
+    resetField(idx) {
+      this.$refs.form.fields.forEach((n) => {
+        if (n.prop.split('.')[1] == idx) {
+          n.resetField()
+        }
+      })
+    },
+    reset() {
+      this.$refs.form.resetFields()
+    },
   },
 }
 </script>
-
-<style lang="scss" scoped>
-.component {
-  font-size: 14px;
-  .component-content {
-    color: #40b883;
-  }
+<style scoped>
+.width100 {
+  width: 100%;
 }
 </style>
